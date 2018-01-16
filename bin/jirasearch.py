16d90720127492e9f1b529fb56973701de8c0a42
Micah Kemp <micah@beatthehell.net>
2018-01-16 16:42:58 +0000

    Template ${earliest} and ${latest} based on the timepicker.

--- bin/jirasearch.py
+++ bin/jirasearch.py
@@ -3,6 +3,7 @@ from splunklib.searchcommands import dispatch, GeneratingCommand, Configuration,
 import sys
 from datetime import datetime
 from jira import JIRA
+from mako.template import Template
 
 @Configuration()
 class JIRAsearchCommand(GeneratingCommand):
@@ -21,8 +22,27 @@ class JIRAsearchCommand(GeneratingCommand):
         for field in self.jira.fields():
             field_id_for_field[field['name']] = field['id']
 
+        # if we can't access search_et, set it to the epoch time
+        try:
+            search_et_epoch = self.search_results_info.search_et
+        except:
+            search_et_epoch = 0.0
+
+        # if we can't access search_lt, set it to now
+        try:
+            search_lt_epoch = self.search_results_info.search_lt
+        except:
+            search_lt_epoch = float(datetime.now().strftime("%s"))
+
+        # format timestamps to be appropriate for JQL
+        search_et_jira = datetime.fromtimestamp(search_et_epoch).strftime("%Y/%m/%d %H:%M")
+        search_lt_jira = datetime.fromtimestamp(search_lt_epoch).strftime("%Y/%m/%d %H:%M")
+
+        # perform substitution
+        templated_query = Template(self.query).render(earliest=search_et_jira, latest=search_lt_jira)
+
         events = []
-        for issue in self.jira.search_issues(self.query, maxResults=self.limit):
+        for issue in self.jira.search_issues(templated_query, maxResults=self.limit):
             event = {
                 '_time': datetime.strptime(issue.fields.created, "%Y-%m-%dT%H:%M:%S.000+0000").strftime("%s"),
                 '_raw': "{}: {}".format(issue.key, issue.fields.summary),
